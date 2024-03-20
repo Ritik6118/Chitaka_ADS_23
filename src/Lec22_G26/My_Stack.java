272863406f6a84c0db2269053a99cad21be2c720
unknown <ritik6118a@gmail.com>
2024-03-20 18:58:58 +0530

    OOPS

--- src/Lec22_G26/My_Stack.java
+++ src/Lec22_G26/My_Stack.java
@@ -1,5 +1,40 @@
 package Lec22_G26;
 
 public class My_Stack {
-
+	protected int [] arr;
+	protected int top;
+	My_Stack(){
+		this.arr=new int[5];
+	}
+	My_Stack(int n){
+		this.arr=new int[n];
+	}
+	public void push(int n) throws Exception {
+		if(this.isFull()) {
+			throw new Exception("Stack is Full");  
+		}
+		arr[top++]=n;
+		
+	}
+	public int pop() throws Exception {
+		if(this.isEmpty()) {
+			throw new Exception("Stack is Empty");
+		}
+		return arr[--top];
+	}
+	public int peek() throws Exception {
+		if(this.isEmpty()) {
+			throw new Exception("Stack is Empty");
+		}
+		return arr[top-1];
+	}
+	public boolean isFull() {
+		return top==arr.length;
+	}
+	public boolean isEmpty() {
+		return top==0;
+	}
+	public int size() {
+		return top;
+	}
 }
--- src/Lec22_G26/My_Stack_Client.java
+++ src/Lec22_G26/My_Stack_Client.java
@@ -4,7 +4,7 @@ public class My_Stack_Client {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
